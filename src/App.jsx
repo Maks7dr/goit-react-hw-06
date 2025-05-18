7655ed9ba9d08be5efe97fc4835489c1ea942dac
Maksym Drachenko <maks7d@gmail.com>
2025-05-18 14:03:35 +0500

    add work

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,14 @@
-import React, { Suspense, lazy } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Navigation from './components/Navigation/Navigation';
-import MovieCast from './components/MovieCast/MovieCast';
-import MovieReviews from './components/MovieReviews/MovieReviews';
+import { Layout } from './components/Layout/Layout';
+import { AppBar } from './components/AppBar/AppBar';
+import { TaskForm } from './components/TaskForm/TaskForm';
+import { TaskList } from './components/TaskList/TaskList';
 
-const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
-const MoviesPage = lazy(() => import('./pages/MoviesPage/MoviesPage'));
-const MovieDetailsPage = lazy(() =>
-  import('./pages/MovieDetailsPage/MovieDetailsPage'),
-);
-const NotFoundPage = lazy(() => import('./pages/NotFoundPage/NotFoundPage'));
-
-export default function App() {
+export const App = () => {
   return (
-    <>
-      <Navigation />
-      <Suspense fallback={<p>Loading...</p>}>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/movies" element={<MoviesPage />} />
-          <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
-            <Route path="cast" element={<MovieCast />} />
-            <Route path="reviews" element={<MovieReviews />} />
-          </Route>
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </Suspense>
-    </>
+    <Layout>
+      <AppBar />
+      <TaskForm />
+      <TaskList />
+    </Layout>
   );
-}
+};
